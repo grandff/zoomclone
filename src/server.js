24fef11da67e0a3fa78f9c265b75aede4681d4cc
root <dsaqaz421@naver.com>
2021-09-23 07:51:37 +0000

    1.2 WebSockets In Nodejs

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 app.set("view engine", "pug");
@@ -8,5 +10,9 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req,res) => res.render("home"));	// home pug로 렌더 처리
 app.get("/*", (req,res) => res.redirect("/"));	// 다른 url 안쓰고 홈만 쓰도록 처리
 
-const handleListen = () => console.log(`Localhost on 3000`);
-app.listen(3000, handleListen);
+const handleListen = () => console.log(`Server on 3000`);
+
+const server = http.createServer(app);		// express application으로부터 서버 생성
+const wss = new WebSocket.Server({server});	// 여기선 동일 포트에서 http, ws 둘다 처리하기 위해 해놨음. 굳이 이렇게 안에 server를 안올려도 됨
+
+server.listen(3000, handleListen);
