395d0be07d94d76236fc6962429d8dfad449b5d2
root <dsaqaz421@naver.com>
2021-09-24 05:50:36 +0000

    1.5 Recap

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,10 +1,12 @@
 // backend와 socket 연결
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = new WebSocket(`wss://${window.location.host}`);
+
+function handleOpen() {
+	console.log("Connected to Server");
+}
 
 // backend에서 소켓이 열려서 통신이 가능할때 확인
-socket.addEventListener("open", () => {
-	console.log("Connected to Server");
-});
+socket.addEventListener("open", handleOpen);
 
 // message 이벤트 추가
 socket.addEventListener("message", (message) => {
--- src/server.js
+++ src/server.js
@@ -15,13 +15,22 @@ const handleListen = () => console.log(`Server on 3000`);
 const server = http.createServer(app);		// express application으로부터 서버 생성
 const wss = new WebSocket.Server({server});	// 여기선 동일 포트에서 http, ws 둘다 처리하기 위해 해놨음. 굳이 이렇게 안에 server를 안올려도 됨
 
+
+// socket이 닫혔을때 호출
+function onSocketClose() {
+	console.log("Disconnected to Browser");
+}
+
+// socket을 통해 메시지 전달
+function onSocketMessage(message){
+	console.log(message.toString("UTF8")); // terminal 에서 깨질 경우 조치
+}
+
 // wss 에서 콜백으로 주는 것들 중에 socket은 통신을 하기 위해 필요하므로 별도로 저장
 wss.on("connection", (socket) => {
 	console.log("Connected to Browser");
-	socket.on("close", () => console.log("Disconnected to Browser"));	// 브라우저가 닫히면 이벤트 발생
-	socket.on("message", message => {
-		console.log(message);
-	});	// 브라우저에서 받은 메시지
+	socket.on("close", onSocketClose);	// 브라우저가 닫히면 이벤트 발생
+	socket.on("message", onSocketMessage);	// 브라우저에서 받은 메시지
 	socket.send("hello!");
 });		// web sockect 연결
 
