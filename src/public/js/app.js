6980000477c268c03179cc3ef91183bb0a8e1981
root <dsaqaz421@naver.com>
2021-09-27 06:55:15 +0000

    1.7 Nicknames part One

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,7 +1,9 @@
 // message가 보여질 ul
 const messageList = document.querySelector("ul");
 // message를 보낼 form
-const messageForm = document.querySelector("form");
+const messageForm = document.querySelector("#message");
+// nickname을 보낼 form
+const nickForm = document.querySelector("#nick")
 // backend와 socket 연결
 const socket = new WebSocket(`wss://${window.location.host}`);
 
@@ -13,8 +15,11 @@ function handleOpen() {
 socket.addEventListener("open", handleOpen);
 
 // message 이벤트 추가
+// message를 받으면 ul에 li가 생겨서 내역을 볼 수 있도록 처리
 socket.addEventListener("message", (message) => {
-	console.log("New Message : " , message.data);
+	const li = document.createElement("li");
+	li.innerText = message.data;
+	messageList.append(li);
 });
 
 // 서버와 통신이 닫혔을 경우
@@ -22,13 +27,29 @@ socket.addEventListener("close", () => {
 	console.log("Disconnected from Server");
 });
 
+// message를 json형태로 변환해서 리턴
+function makeMessage(type, payload){
+	const msg = {type, payload};
+	return JSON.stringify(msg);		// 백엔드마다 다른 언어를 사용하므로 json object가 아닌 string 형태로 리턴해줘야함
+}
+
 // submit functions
 function handleSubmit(event){
 	event.preventDefault();
 	const input = messageForm.querySelector("input");	// message 입력창
-	socket.send(input.value);		// messabe를 server로 전달
+	socket.send(makeMessage("new_message", input.value));		// messabe를 server로 전달
+	input.value = "";
+}
+
+// nick form submit function
+function handleNickSubmit(event){
+	event.preventDefault();
+	const input = nickForm.querySelector("input");	// nickname 입력창
+	socket.send(makeMessage("nickname", input.value));
 	input.value = "";
 }
 
 // message form add submit events
 messageForm.addEventListener("submit", handleSubmit);
+// nick form add submit events
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ const handleListen = () => console.log(`Server on 3000`);
 
 const server = http.createServer(app);		// express application으로부터 서버 생성
 const wss = new WebSocket.Server({server});	// 여기선 동일 포트에서 http, ws 둘다 처리하기 위해 해놨음. 굳이 이렇게 안에 server를 안올려도 됨
+const sockets = [];	// 서버에 연결한 소켓 정보를 저장할 임시 저장소
 
 
 // socket이 닫혔을때 호출
@@ -26,12 +27,16 @@ function onSocketMessage(message){
 	//console.log(message.toString("UTF8")); // terminal 에서 깨질 경우 조치	
 }
 
+// utf8 변환
+const changeUTF8 = (str) => str.toString("UTF8");
+
 // wss 에서 콜백으로 주는 것들 중에 socket은 통신을 하기 위해 필요하므로 별도로 저장
-wss.on("connection", (socket) => {
+wss.on("connection", (socket) => {	
 	console.log("Connected to Browser");
+	sockets.push(socket);	// 연결한 소켓 저장
 	socket.on("close", onSocketClose);	// 브라우저가 닫히면 이벤트 발생
 	socket.on("message", (message) => {
-		socket.send(message.toString("UTF8"));
+		sockets.forEach(aSocket => aSocket.send(changeUTF8(message)));		// 저장된 모든 소켓에 foreach를 사용해서 다 보내줌
 	});	// 브라우저에서 받은 메시지
 	
 });		// web sockect 연결
